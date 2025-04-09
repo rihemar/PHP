603444a394510a0c33271422429d02c0c3c7efeb
unknown <rihemar2412@gmail.com>
2025-04-09 12:58:48 +0100

    index.php commit

---
+++ EXERCICE1/index.php
@@ -0,0 +1,16 @@
+86% of storage used … If you run out, you can't create, edit, and upload files. Get 100 GB of storage for $1.99 $0.99/month for 6 months.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> 
+    <title>Document</title>
+</head>
+<body>
+<?php include("Etud.php");?>
+    
+</body>
+<script src="script.js"></script>
+
+</html>
